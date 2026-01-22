5783b6d92a64ebbd74cec15c88295fa9d39c6842
SakshamKriplani <kriplanisaksham.31@gmail.com>
2026-01-23 02:44:45 +0530

    Initial commit

---
+++ src/main/java/com/example/imageconverter/ImageconverterApplication.java
@@ -0,0 +1,13 @@
+package com.example.imageconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageconverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageconverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/imageconverter/ImageconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imageconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
